6b69d86a2ea5f81b902ab86127babea9db0cac90
qizai <1438890589@qq.com>
2021-10-25 22:57:46 +0800

    third commit

--- src/com/wch/Test01.java
+++ src/com/wch/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World2!");
+        System.out.println("Hello World3!");
     }
 }
