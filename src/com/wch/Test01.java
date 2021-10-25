e49ccbc8e60ebe92ab38ddb521ceee2241bea857
qizai <1438890589@qq.com>
2021-10-25 22:57:02 +0800

    second commit

--- src/com/wch/Test01.java
+++ src/com/wch/Test01.java
@@ -3,5 +3,6 @@ package com.wch;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
